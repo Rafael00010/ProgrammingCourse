9dddd14956826a36a99298ad64cbc44ecf46dbe4
Rafael00010 <rafael00010@gmail.com>
2023-07-27 12:28:49 +0200

    Added Section17 challenge and MoveCtor example

--- ProgrammingCourse/ProgrammingCourse.cpp
+++ ProgrammingCourse/ProgrammingCourse.cpp
@@ -13,11 +13,14 @@ int main()
     //std::cout << sum_of_digits(1234567890);
     //SectionElevenChallenge();
     //SectionTwelveChallenge();
+    
+    MoveCtorS13();
     //Section13Challenge();
     //Section14ChallengeS1();
     //Section15Challenge();
+    //Section16Challenge();
 
-    Section16Challenge();
+    //Section17Challenge();
 
     int end{};
     std::cin >> end;
